83d3ab5615a4bd4e53c14af679465204a438eef5
raoulpicconi <raoulpicconi03@gmail.com>
2023-10-05 17:59:03 +0200

    fix: adjusted the deploy script

--- script/Lending.s.sol
+++ script/Lending.s.sol
@@ -13,15 +13,16 @@ contract DeployLending is Script {
     function run() external {
         address GOVERNANCE_TREASURY_ADDRESS = vm.envAddress("GovernanceTreasuryAddress");
         address PRICE_INDEX_ADDRESS = vm.envAddress("PriceIndexAddress");
-        uint256 protocolFee = 15000; // 1.5%
+        address TREASURY_MANAGER = vm.envAddress("TreasuryManager");
+        uint256 protocolFee = 150; // 1.5%
         uint256 repayGracePeriod = 60 * 60 * 24 * 5; // 5 days
-        uint256 repayGraceFee = 25000; // 2.5%
-        uint256 feeReductionFactor = 14000; // 1.4%
+        uint256 repayGraceFee = 250; // 2.5%
+        uint256 feeReductionFactor = 14000; // 140%
         uint256[] memory originationFeeRanges = new uint256[](3);
-        originationFeeRanges[0] = 50000e18; // 50k
-        originationFeeRanges[1] = 100000e18; // 100k
-        originationFeeRanges[2] = 500000e18; // 500k
-        uint256 liquidationFee = 50000; // 5%
+        originationFeeRanges[0] = 50_000; // 50k
+        originationFeeRanges[1] = 100_000; // 100k
+        originationFeeRanges[2] = 500_000; // 500k
+        uint256 liquidationFee = 500; // 5%
         uint256[] memory durations = new uint256[](5);
         durations[0] = WEEK_1;
         durations[1] = MONTHS_1;
@@ -29,12 +30,12 @@ contract DeployLending is Script {
         durations[3] = 6 * MONTHS_1;
         durations[4] = 12 * MONTHS_1;
         uint256[] memory interestRates = new uint256[](5);
-        interestRates[0] = 66000;
-        interestRates[1] = 73000;
-        interestRates[2] = 80000;
-        interestRates[3] = 88000;
-        interestRates[4] = 97000;
-        uint256 baseOriginationFee = 10000; // 1%
+        interestRates[0] = 660; // 6.6%
+        interestRates[1] = 730; // 7.3 %
+        interestRates[2] = 800; // 8 %
+        interestRates[3] = 880; // 8.8 %
+        interestRates[4] = 970; // 9.7 %
+        uint256 baseOriginationFee = 100; // 1%
 
         uint256 deployerPrivateKey = vm.envUint("PRIVATE_KEY");
         console.log("Deploying Lending contract...");
@@ -52,6 +53,7 @@ contract DeployLending is Script {
             interestRates,
             baseOriginationFee
         );
+        lending.grantRole(lending.TREASURY_MANAGER_ROLE(), TREASURY_MANAGER);
         vm.stopBroadcast();
         console.log("Lending contract successfully deplyed at: ", address(lending));
     }
